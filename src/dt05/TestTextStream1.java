da39157b41acd05723e55ba67d888025982d49fc
Yidong Cui <nathan.cui@gmail.com>
2017-11-22 13:15:13 +0800

    双培第五次培训内容

---
+++ src/dt05/TestTextStream1.java
@@ -0,0 +1,17 @@
+package dt05;
+
+import java.io.File;
+
+/* Ex.1
+ * 1. 创建一个File对象，打开硬盘上的文件Example.txt
+ * 2. 判断Example.txt是否存在，输出存在与否的信息
+ */
+public class TestTextStream1 {
+
+  public static void main(String[] args) {
+    File f = new File("Example.txt");
+    boolean exists = f.exists();
+    System.out.println(exists);
+  }
+
+}
